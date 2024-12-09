8a8054609c74df51d3141c003c8f2a4ba4136966
Anton Tikhonov <free.tikhonov@gmail.com>
2024-12-09 23:30:41 +0300

    add edge-to-edge

--- composeApp/src/androidMain/kotlin/ru/antontikhonov/timetable_kmp/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/ru/antontikhonov/timetable_kmp/MainActivity.kt
@@ -2,16 +2,24 @@ package ru.antontikhonov.timetable_kmp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.remember
 import io.ktor.client.engine.okhttp.OkHttp
 import ru.antontikhonov.timetable_kmp.networking.TimetableClient
 import ru.antontikhonov.timetable_kmp.networking.createHttpClient
 import ru.antontikhonov.timetable_kmp.presentation.App
+import ru.antontikhonov.timetable_kmp.resources.Colors
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.dark(
+                scrim = Colors.DARK_BLACK_TRANSPARENT.value.toInt(),
+            )
+        )
         super.onCreate(savedInstanceState)
 
         setContent {
